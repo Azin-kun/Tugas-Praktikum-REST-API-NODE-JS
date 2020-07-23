4ba9bb7ac5cf7d456a5868a7cd1b6016c16f7c44
Azin <62732881+Azin-kun@users.noreply.github.com>
2020-07-24 06:37:17 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -10,17 +10,6 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(cors())
 
-app.get("/test", (req,res) => {
-
-    let response = {
-        message: "Ini end-point pertama ku",
-        method: req.method,
-        code: res.statusCode
-    }
-
-    res.json(response)
-})
-
 app.listen(8000, () => {
     console.log("Server run on port 8000");
 })
